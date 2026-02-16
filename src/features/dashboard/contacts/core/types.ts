0fc9b4d7c8b428661e70756fbed7077392f8bc4d
MohamadAliAsadi <MohamadAliAsadi.ir@gmail.com>
2026-02-16 15:08:10 +0330

    fix: contact form config

--- src/features/dashboard/contacts/core/types.ts
+++ src/features/dashboard/contacts/core/types.ts
@@ -1,27 +1,42 @@
 import type { FilterValue } from "@/features/shared/ui/table";
+import { positionLabels } from "./../configs/table.config";
+import { ContactPosition } from "@/api/types";
 
 export interface ContactFormData {
-	[key: string]: string | undefined;
-	id?: string;
-	organName?: string;
-	lastName?: string;
-	mobile?: string;
-	status?: string;
-	email?: string;
-	companyName?: string;
-	note?: string;
+  [key: string]: string | undefined;
+  id?: string;
+  firstName?: string;
+  lastName?: string;
+  phone?: string;
+  status?: string;
+  email?: string;
+  companyId?: string;
+  position?: ContactPosition;
+  note?: string;
+}
+
+export interface ContactTableRow {
+  [key: string]: string | number | undefined;
+  id: number;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  position: ContactPosition;
+  email: string;
+  createdAt: string;
+  assignedToUserId: string;
 }
 
 export type FormMode = "create" | "edit" | "view";
 
 export interface ContactsState {
-	currentPage: number;
-	filters: Record<string, FilterValue>;
-	sortField: string | null;
-	sortOrder: "asc" | "desc" | null;
-	selectedIds: number[];
-	isFormOpen: boolean;
-	formMode: FormMode | null;
-	formInitialValues: Record<string, string> | null;
-	selectedFilter: string;
+  currentPage: number;
+  filters: Record<string, FilterValue>;
+  sortField: string | null;
+  sortOrder: "asc" | "desc" | null;
+  selectedIds: number[];
+  isFormOpen: boolean;
+  formMode: FormMode | null;
+  formInitialValues: Record<string, string> | null;
+  selectedFilter: string;
 }
